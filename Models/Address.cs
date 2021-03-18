fbcd8edf1b2282d86fe6711b35cf9e118a9f3de9
ProgramistycznyŚwir <programistycznyswir@gmail.com>
2021-03-18 10:41:29 +0100

    Dodaj pliki projektów.

---
+++ Models/Address.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Models
+{
+    public class Address
+    {
+        [Display(Name = "Ulica")]
+        [Required(ErrorMessage = "You have to live on some street.")]
+        [StringLength(32, MinimumLength = 3)]
+        public string Street { get; set; }
+
+        [Display(Name = "Kod pocztowy")]
+        [Required(ErrorMessage = "This street should have it's zipcode.")]
+        [StringLength(32, MinimumLength = 3)]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Miasto")]
+        [Required(ErrorMessage = "City field is required.")]
+        public string City { get; set; }
+
+        [Display(Name = "Numer telefonu")]
+        [Required(ErrorMessage = "You should specify propper number.")]
+        public int Number { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using HelloWorld.Models;
+
+namespace HelloWorld.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        //[BindProperty(SupportsPost = true)]
+        [BindProperty]
+        public Address Address { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                Name = "User";
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+            return RedirectToPage("./Privacy");
+        }
+    }
+}
