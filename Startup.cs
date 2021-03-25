ca932ee59c3aa1e1ac13f6a116075092f739a1f6
ProgramistycznyŚwir <programistycznyswir@gmail.com>
2021-03-25 11:04:18 +0100

    Kod po zmianach zaproponowanych w tutorialu

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,17 @@ namespace HelloWorld
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            // Zadanie 3:
+            services.AddMemoryCache();
+            //services.AddDistributedMemoryCache();
+            services.AddSession();
+            //services.AddSession(options =>
+            //{
+            //    options.IdleTimeout = TimeSpan.FromSeconds(10);
+            //    options.Cookie.HttpOnly = true;
+            //    options.Cookie.IsEssential = true;
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +62,9 @@ namespace HelloWorld
             {
                 endpoints.MapRazorPages();
             });
+
+            // Zadanie 3:
+            app.UseSession();
         }
     }
 }
